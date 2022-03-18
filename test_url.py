b788db6f347711ed7b2547604a30bad829e24a1b
Marita <101862803+MaritaBaro@users.noreply.github.com>
2022-03-18 11:18:54 +0300

    Add files via upload

---
+++ test_url.py
@@ -0,0 +1,154 @@
+# import sys
+
+title = 'The Meaning of Life'
+print(title)
+# print(sys.platform)
+# print(2 ** 8)
+# x = 'Spam!'
+# print(x * 8)
+# input()
+from pprint import pprint
+# person = 45
+#
+# if person < 2:
+#     print('little baby')
+# elif 2 < person < 4:
+#     print('baby')
+# elif 13 < person < 20:
+#     print('teenager')
+# elif 20 < person < 65:
+#     print('adult')
+# else:
+#     print('old')
+#import value as value
+
+#familiar ={'first_name': 'Serg', 'last_name': 'Lukash', 'age': 38, 'city': 'Kiev'}
+
+# for k,v in familiar.items():
+#     # //print(f"\n {k} : {v}")
+#     print(k,':', v)
+
+# sw_pet = {'cat': 'Svitlana'}
+# ksu_pet = {'bird': 'Happy'}
+# pets = []
+# pets.append(sw_pet)
+# pets.append(ksu_pet)
+# for pet in pets:
+#     pprint(pet)
+
+
+# name_person = 'Piter'
+# name_person2 = '\tmax \n m'
+
+# world_place = ['China', 'London', 'Hawai', 'Mexica', 'Dominicana']
+# best_pizza = ['Paperoni', 'Four cheese', 'Muschrooms']
+# menu_cafe = ('Soup', 'Ice-cream', 'Bread', 'Salad', 'Water', 'Lemon')
+#
+# freind_list = ['admin', 'Nim', 'Garry', 'Ann', 'Fiona', 'Like', 'Nora', 'Lin', 'Roman', 'Fenix']
+# empty_list = []
+#
+# if empty_list == []:
+#     print('The list is empty')
+# else:
+#     print('NOT empty')
+#
+# for friend in freind_list:
+#     if friend == 'admin':
+#         print('Hello, admin. Do you want to see a report?')
+#     else:
+#         print('Hello, ' + friend + ' thank for loggin in again')
+
+# number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# for number in number_list:
+#     if number == 1:
+#         print(str(number)+'st')
+#     elif number == 2:
+#         print(str(number)+'nd')
+#     elif number == 3:
+#         print(str(number)+'rd')
+#     else:
+#         print(str(number)+'th')
+
+# current_users = ['Bro', 'Funny cap', 'Dirty girl', 'Hungry cake', 'Ironman']
+# march_current_users = ['Pinky', 'Ronni', 'BRO', 'Noisy party', 'Ironman']
+#
+# for user in march_current_users:
+#     if user in current_users:
+#         print('You must change name' + user)
+#     elif user.capitalize() in current_users:
+#         print('You must name' + user)
+#     else:
+#         print('Good name.')
+
+# alien_color = ['green']
+#
+# if alien_color == 'green':
+#     print('You get 5 points')
+# elif alien_color != 'green':
+#     print('You get 10')
+# else:
+#   print('You are lose')
+
+# list_number = []
+# for my_number in range(1, 11):
+#     square = my_number ** 2
+#     list_number.append(square)
+# print(list_number)
+#
+# print([my_number ** 2 for my_number in range(1, 11)])
+
+
+# for pizza in best_pizza:
+#     print('I like '+ pizza)
+# print('Do you want to know what pizza I like? Ha! I like paperoni.')
+
+# print(for pizza in best_pizza)
+
+
+# world_place.reverse()
+# print(world_place)
+# print(world_place)
+# world_place.reverse()
+# print(world_place)
+# world_place.sort()
+# print(world_place)
+# world_place.reverse()
+# world_place.sort()
+# print(world_place)
+
+# for friend in (freind_list[0:4]):
+#     print(friend + ', sorry, I can\'t invite you.')
+#
+# for friend in (freind_list[3:5]):
+#     print(friend + ', I invite you to the holiday')
+#
+# print(freind_list)
+#
+# for i in range(len(freind_list)):
+#     print(freind_list.pop())
+#
+# print(freind_list)
+
+
+# from urls_utils import gen_from_urls
+#
+# urls = ('http://head.com', 'http://teddy.com', 'http://love.com')
+#
+# for resp_len, status, url, in gen_from_urls(urls):
+#     print(resp_len, '->', status, '->', url)
+#
+
+# from turtle import *
+#
+# color('purple', 'cyan')
+# begin_fill()
+#
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 1:
+#         break
+#
+# end_fill()
+# done()
